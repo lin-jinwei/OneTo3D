bed9070cef03656a695363e6790fcbd5eb1ace6e
lin-jinwei <165241983+lin-jinwei@users.noreply.github.com>
2024-04-25 20:44:45 +0800

    update vidoeToGif.py
    lin-jinwei

--- vidoeToGif.py
+++ vidoeToGif.py
@@ -1,9 +1,18 @@
 from moviepy.editor import *
 import os
+import argparse
 
-video_name = '0001-0396.mkv'
-video_path = os.path.join('./output3D/3Dvideo/', video_name) 
-gif_path = os.path.join('./output3D/gifs/', video_name.split('.')[0]+'.gif') 
 
-gif_clip = VideoFileClip(video_path)
-gif_clip.write_gif(gif_path)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Change video to gif image.')
+    parser.add_argument("--videoName", required=True, default='', help="The video name in ./output3D./3Dvideo")
+
+    args = parser.parse_args()
+
+    # video_name = '0001-0396.mkv'
+    video_name = args.videoName
+    video_path = os.path.join('./output3D/3Dvideo/', video_name) 
+    gif_path = os.path.join('./output3D/gifs/', video_name.split('.')[0]+'.gif') 
+
+    gif_clip = VideoFileClip(video_path)
+    gif_clip.write_gif(gif_path)
