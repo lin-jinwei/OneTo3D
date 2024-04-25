6920c4ac3cfd6101fa3e6a0ce3102091e238a64e
lin-jinwei <165241983+lin-jinwei@users.noreply.github.com>
2024-04-25 20:45:59 +0800

    update animation.py
    lin-jinwei

--- animation.py
+++ animation.py
@@ -2,6 +2,7 @@
 # Time: 06, Apirl, 2024 
 
 import re
+import argparse
 
 # def get_index(astr, subStr):
 #     try:
@@ -320,6 +321,14 @@ def animation(command):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate the commands list.')
+    parser.add_argument("--command", required=True, default='', help="text of the command.")
+
+    args = parser.parse_args()
+
+    animation(args.command)
+
+
     # command = 'The object moves 2 miles in x direction. '
     # command = 'The object moves 2 miles in -x direction.'
     # command = 'The object ran 3 steps tiems seconds to the car'
@@ -393,20 +402,9 @@ if __name__ == '__main__':
     command = 'Big: The object raises his head 30 degrees, bowed his head 20 degrees, and shakes his head 60 degrees, looks left 22.33 degrees, looks right 22.33 degrees, walked 2 steps tiems and ran 3 steps tiems seconds, raised his left hand 20 degrees, raised his right hand 20 degrees, put down his left hand 30 degrees, put down his right hand 30 degrees, waves his left hand 60 degrees, waves his right hand 60 degrees, lift his left leg 60 degrees, lift his right leg 60 degrees, puts down his left leg 60 degrees, puts down his right leg 60 degrees, puts down his left leg, lifts his left leg, raises his left forearm 30 degrees, raises his right forearm 30 degrees, puts down his left forearm 30 degrees, puts down his right forearm 30 degrees, wave his left forearm 45 degrees, wave his right forearm 45 degrees, puts down his left calf 20 degrees, puts down his right calf 20 degrees, lifts his left calf 20 degrees, lifts his right calf 20 degrees, puts down his left calf, puts down his right calf, lifts his left calf, lifts his right calf, to the car'
 
 
-
-
-
-
-
-
-
-
-
-
-
     
     
-    animation(command)
+    # animation(command)
 
 
 
